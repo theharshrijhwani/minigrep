911eae51fa6cdc354b22b4747df50186b975aa88
theharshrijhwani <110522542+theharshrijhwani@users.noreply.github.com>
2024-01-20 18:49:00 +0530

    main.rs done. starting with lib.rs

---
+++ src/main.rs
@@ -0,0 +1,45 @@
+use std::env;
+use std::fs;
+use std::process;
+use std::error::Error;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let config =  Config::build(&args).unwrap_or_else(|err| {
+        println!("problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    println!("path: - {}", config.file_path);
+    println!("query: - {}", config.query);
+    
+    if let Err(e) = run(config) {
+        println!("application error: - {e}");
+        process::exit(1);
+    }
+
+    
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+    println!("{}", contents);
+    Ok(())
+}
+
+struct Config {
+    query: String,
+    file_path: String
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len()<3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config {query, file_path})
+    }
+}
